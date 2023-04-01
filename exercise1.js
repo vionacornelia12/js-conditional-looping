4080b0244acab34824d2c3c8653d2d4be5b6653c
Viona Cornelia <vionacornelia12@gmail.com>
2023-04-01 21:20:08 +0700

    add exercise1.js

---
+++ exercise1.js
@@ -0,0 +1,7 @@
+// Kita sudah mengetahui bahwa banyaknya user maksimal adalah 100.
+// Buat sebuah program yang menampilkan teks ‘User ke - 1 … User ke - 100’ pada setiap baris di halaman HTML.
+// Lakukan FOR LOOP pada Javascript.
+
+for (let i = 1; i <= 100; i++) {
+  document.write("User ke - " + i + "<br>");
+}
