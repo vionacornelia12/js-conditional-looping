6993a57b447eda8403ea5594c2c759bd759d2207
Viona Cornelia <vionacornelia12@gmail.com>
2023-04-01 21:20:34 +0700

    add exercise2.js

---
+++ exercise2.js
@@ -0,0 +1,12 @@
+// Lakukan pengulangan menggunakan FOR LOOP untuk melakukan penambahan nilai sebanyak 10 kali.
+// Nilai awal = 0
+// Pengulangan = 10 kali
+// Nilai awal ditambah 2 setiap pengulangan
+// Tampilan hasil penambahan pada setiap pengulangan
+
+let nilai_awal = 0;
+
+for (let i = 0; i < 10; i++) {
+  nilai_awal += 2;
+  console.log("Nilai saat ini: " + nilai_awal);
+}
